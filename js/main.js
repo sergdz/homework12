b987808021206c0428aaf3e835e204625b81a41b
sergdz <sergeydzubenko1@gmail.com>
2022-02-17 01:08:05 +0200

    added burger

--- js/main.js
+++ js/main.js
@@ -1,36 +1,24 @@
-var imgs = document.querySelectorAll(".slider slider__items");
-console.log(imgs);
-
-var next = document.querySelector(".slider .slider__btn--secondary");
-console.log(next)
-
-var prev = document.querySelector(".slider .slider__btn--primary");
-console.log(prev)
-
-var primary_slide = 0;
+var modal = document.getElementById('myModal');
 
 
-next.addEventListener("click", function(){
-    imgs[current_slide].style.opacity = 0;
-});
+var btn = document.getElementById("header__burger");
 
-next.addEventListener("click", function(){
-    imgs[primary_slide].style.opacity = 0;
-    if(primary_slide == imgs.length-1) {
-        current_slide = 0
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
-    else {
-        primary_slide++;
-    }
-    imgs[primary_slide].style.opacity = 1;
-})
-
-prev.addEventListener("click", function(){
-    imgs[primary_slide].style.opacity = 0;
-    if(current_slide == 0) {
-        primary_slide = imgs.length-1
-    } else {
-        primary_slide--;
-    }
-    imgs[current_slide].style.opacity = 1;
-} )
+}
